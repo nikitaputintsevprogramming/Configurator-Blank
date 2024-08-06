47a7946c5974e3ae70b4c1b1a25ea0a16ef3da44
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2024-08-06 10:06:13 +0300

    Change unity version

---
+++ Assets/Scripts/ControlVersions/EditorGUILayoutToggle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.TerrainTools;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.ControlVersions
+{
+    public class EditorGUILayoutToggle : MonoBehaviour // UnityEditor.EditorWindow
+    {
+        
+    }
+}
