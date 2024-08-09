33b8679ab6927161cd130ccc3235625db2941ac8
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2024-08-09 11:52:53 +0300

    уборка

--- Assets/Scripts/ControlVersions/ButtonsRotate/ArrowsTest.cs
+++ Assets/Scripts/ControlVersions/ButtonsRotate/ArrowsTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 using System;
-using Assets.Scripts.ControlVersions.ButtonsRotate;
+using System.Security.Cryptography;
 
 namespace ArrowControl
 {
@@ -15,9 +15,9 @@ namespace ArrowControl
     };
 
     [ExecuteAlways]
-    public class ArrowsData : MonoBehaviour
+    public class ArrowsTest : MonoBehaviour
     {
-        public static ArrowsData Instance;
+        public static ArrowsTest Instance;
 
         static Dictionary<int, string> sides = new Dictionary<int, string>()
         {
@@ -41,6 +41,11 @@ namespace ArrowControl
         [Range(1, 4)]
         public int _amountButtons;
 
+        //components
+        public GridLayoutGroup gridGroup;
+        public Canvas canvas;
+        public CanvasScaler canvasScaler;
+
         public void OnValidate()
         {
             Reset();
@@ -61,16 +66,17 @@ namespace ArrowControl
             {
                 //Debug.Log("Нет канваса");
                 CreateCanvas();
+                GetCanvasComponents();
             }
-            if(_buttons.Count < _amountButtons)
+            if (_buttons.Count < _amountButtons)
             {
+                ResetCanvasComponent();
                 AddButtons();
-                AddComponents();
             }
         }
 
         [ContextMenu("Reset Canvas")]
-        void DeleteExcess()
+        public void DeleteExcess()
         {
             // Производим итерацию задом наперед, так как RemoveAt пропускает данные в единице
             // https://ru.stackoverflow.com/questions/992441
@@ -91,33 +97,36 @@ namespace ArrowControl
             }
         }
 
-        private void CreateCanvas()
+        public void CreateCanvas()
         {
             // Создание Canvas
-            _canvasObj = new GameObject("canvasArrows", typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster));
+            _canvasObj = new GameObject("canvasArrows", typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster), typeof(GridLayoutGroup));
             _canvasObj.transform.SetParent(transform);
         }
 
-        private void AddComponents()
-        {
-            Canvas canvas = _canvasObj.GetComponent<Canvas>();
-            CanvasScaler canvasScaler = _canvasObj.GetComponent<CanvasScaler>();
-            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-            canvasScaler.referenceResolution = new Vector2(_resolutionScreen.x, _resolutionScreen.y);
-            canvasScaler.matchWidthOrHeight = 0.5f;
-
-            GridLayoutGroup gridGroup = _canvasObj.AddComponent<GridLayoutGroup>();
-            gridGroup.childAlignment = TextAnchor.LowerCenter;
-        }
-
-        private void AddButtons()
+        public void AddButtons()
         {
             for (int i = _buttons.Count; _buttons.Count < _amountButtons; i++)
             {
                 _buttons.Add(new GameObject("Arrow" + sides[i], typeof(Image), typeof(Button)));
                 _buttons[i].transform.SetParent(_canvasObj.transform);
             }
+        }
+
+        public void GetCanvasComponents()
+        {
+            canvas = _canvasObj.GetComponent<Canvas>();
+            canvasScaler = _canvasObj.GetComponent<CanvasScaler>();
+            gridGroup = _canvasObj.GetComponent<GridLayoutGroup>();
+        }
+
+        public void ResetCanvasComponent()
+        {
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.referenceResolution = new Vector2(_resolutionScreen.x, _resolutionScreen.y);
+            canvasScaler.matchWidthOrHeight = 0.5f;
+            gridGroup.childAlignment = TextAnchor.LowerCenter; 
         }
 
         private void OnDestroy()
