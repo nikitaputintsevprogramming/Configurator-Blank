6a62d07892178416830811f74dd7968220ee68ec
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2024-08-22 13:58:08 +0300

    Добавлены пакеты для Lean Touch and GUI

---
+++ Assets/CameraPresets/Scripts/Tools/AddCameraPreset.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace CameraPreset
+{
+    public class AddCameraPreset
+    {
+        [MenuItem("Tools/Do Something")]
+        static void DoSomething()
+        {
+            Debug.Log("Doing Something...");
+        }
+    }
+}
